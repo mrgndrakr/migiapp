d2bfcf9d6c50b4bf38f0cf3e41da6613d4ab4247
mrigendra <mrgndrakr@gmail.com>
2013-02-24 15:11:00 +0530

    create a rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Migiapp::Application.config.secret_token = '93321790bd8fb208c9a0eac8ac0c438944d2ab3df4f3b17b6e48deb36925394e4b7b9e9b676a3e9ce532f63d8ae0a5616ec485eacaca689942d7d5ecc7077114'
